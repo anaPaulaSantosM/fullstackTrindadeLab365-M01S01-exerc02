a3de85ebaa824a0c213c05847f907f415064bfad
Ana Paulo dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-03-23 09:39:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var exec2 = "->Exercício 2: [App SaveCollege] Média dos alunos"
+console.log(exec2);
+var media = aluno[0].matematica + aluno[2].matematica + aluno[3].matematica + aluno[4].matematica / 5;
+console.log(`A média dos alunos é: ${media}`);
